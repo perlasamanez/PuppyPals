68d51f7cfd99a92642fb999108bae8ade5d9ff90
Julia Warner <juliawarner@Julias-MacBook-Pro-4.local>
2019-04-16 00:09:26 -0400

    age and radiusSeeking now return as strings

--- API/login.php
+++ API/login.php
@@ -6,24 +6,36 @@
 	$firstName = "";
 	$lastName = "";
 	$email = "";
+    $dName = "";
+    $size = -1;
+    $breed = "";
+    $age = -1;
+    $bio = "";
+    $gender = "";
+    $imgDog = "";
+    $sizeSeeking = -1;
+    $radiusSeeking = -1;
+    $genderSeeking = "";
+
 	$username = $inData["username"];
 	$password = $inData["password"];
     
     $conn = new mysqli("localhost", "poopspg2_user_pp", "FYPykYr~@7T!", "poopspg2_PuppyPals");
-
+    
     if($conn->connect_error)
     {
         returnWithError( $conn->connect_error );
     }
     else
     {
-	#$salted = "alk3245jylj345j345g34fg6532l1".$password."lksdfjklerwwej23g42g34g234g32";
-
-	#$hashed = hash('sha512', $salted);
         
-	$sql = "select userID from user where username ='" . $username . "' and password ='" . $password . "'";
-
+        $salted = "alk3245jylj345j345g34fg6532l1".$password."lksdfjklerwwej23g42g34g234g32";
+        $hashed = hash('sha512', $salted);
+        
+        $sql = "SELECT * FROM user WHERE username ='" . $username . "' and password ='" . $hashed . "'";
+        
         $result = $conn->query($sql);
+        
         if($result->num_rows > 0)
         {
             $row = $result->fetch_assoc();
@@ -31,10 +43,30 @@
 			$firstName = $row["firstName"];
             $lastName = $row["lastName"];
             $email = $row["email"];
+
+            //get dog info
+            $sql = "SELECT * FROM dog WHERE userID=" . $userID;
+            $dog_query = $conn->query($sql);
+            $dog = $dog_query->fetch_assoc();
+            $dName = $dog["dName"];
+            $size = $dog["size"];
+            $breed = $dog["breed"];
+            $age = $dog["age"];
+            $bio = $dog["bio"];
+            $gender = $dog["gender"];
+            $imgDog = $dog["imgDog"];
+
+            //get settings info
+            $sql = "SELECT * FROM settings WHERE userID=" . $userID;
+            $settings_query = $conn->query($sql);
+            $settings = $settings_query->fetch_assoc();
+            $sizeSeeking = $settings["sizeSeeking"];
+            $radiusSeeking = $settings["radiusSeeking"];
+            $genderSeeking = $settings["genderSeeking"];
             
             $conn->close();
             
-            returnWithInfo($userID, $firstName, $lastName, $email);
+            returnWithInfo($userID, $firstName, $lastName, $email, $dName, $size, $breed, $age, $bio, $gender, $imgDog, $sizeSeeking, $radiusSeeking, $genderSeeking);
         }
         else
         {
@@ -61,9 +93,9 @@
         sendResultInfoAsJson( $retValue );
     }
     
-    function returnWithInfo( $firstName, $lastName, $userID )
+    function returnWithInfo($userID, $firstName, $lastName, $email, $dName, $size, $breed, $age, $bio, $gender, $imgDog, $sizeSeeking, $radiusSeeking, $genderSeeking)
     {
-        $retValue = '{"userID":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","email":"' . $email . '","error":""}';
+        $retValue = '{"userID":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","email":"' . $email . '","dName":"' . $dName . '","size":' . $size . ',"breed":"' . $breed . '","age":"' . $age . '","bio":"' . $bio . '","gender":"' . $gender . '","imgDog":"' . $imgDog . '","sizeSeeking":' . $sizeSeeking . ',"radiusSeeking":"' . $radiusSeeking . '","genderSeeking":"' . $genderSeeking . '","error":""}';
         sendResultInfoAsJson( $retValue );
     }
     
